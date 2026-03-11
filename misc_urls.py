9afda2667888e47c3b1b56f12902d50337e36715
mekererenate-web <mekererenate@gmail.com>
2026-03-11 20:00:30 +0100

    Update misc_urls.py
    group e dashboard

--- misc_urls.py
+++ misc_urls.py
@@ -25,19 +25,17 @@ GROUP_URLS = [
     ],
     [
         "group d",
-        "Mixed",
+        "<name>",
         # Monitoring URL:
-        # This Grafana instance is currently running on a VM that we set up for staging, so it is not showing the metrics from the real simulator.
-        # Once we have all of the components ready, we'll apply it to the production server and update the URL here accordingly.
-        "http://134.122.71.241:3000/public-dashboards/755d9f514e484aa6b762b721edb282fb",
+        "<monitoring_url>",
         # Logging URL:
         "<logging_url>",
     ],
     [
         "group e",
-        "<name>",
+        "Group E Dashboard Minitwit",
         # Monitoring URL:
-        "<monitoring_url>",
+        "http://209.38.209.29:3000/public-dashboards/c13660544ba1472688314005db4ba3cc",
         # Logging URL:
         "<logging_url>",
     ],
